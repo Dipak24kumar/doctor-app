6b8179c787492d2a27821abfd39bfc7713b6acdb
Dipak24kumar <dipakkumar@cedcommerce.com>
2022-10-29 12:54:03 +0530

    add webapp

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import './assets/css/Reset.css';
 import './assets/css/global.css'; 
-
 import Page_1 from './pages/Page_1.js';
+import Page_2 from './pages/Page_2.js';
  
 function App() {
   return (
     <div className="App-cover"> 
-     <Page_1 /> 
+     {/* <Page_1 /> */}
+     <Page_2 />
+
     </div> 
   );
 }
---
+++ src/pages/Page_2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Page_2() {
+  return (
+    <section className='page-cover text-center'>
+      <h1 className='mb-4'>What will we learn</h1>
+      <button className='default-btn'>What is CPR ?</button>
+    </section>
+  ) 
+}
+
+export default Page_2;
