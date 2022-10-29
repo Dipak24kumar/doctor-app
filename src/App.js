a4f80c988d4fbdbd64899214ee7ffd4f3f2b26ee
Dipak24kumar <dipakkumar@cedcommerce.com>
2022-10-29 18:33:37 +0530

    bhupendra 29 oct

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import './App.css';
+// import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './assets/css/Reset.css';
 import './assets/css/global.css';
 import Page_1 from './pages/Page_1.js';
-// import Page_2 from './pages/Page_2.js';
+import Page_2 from './pages/Page_2.js';
 // import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App-cover">
-      {/* <Page_1 /> */}
-      <Page_1 />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Page_1 />}>
+          {/* <Route
+            path="/home"
+            element={<Page_1 />}
+          /> */}
+          <Route path="/Page_2" element={<Page_2 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
