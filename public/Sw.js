9fbf4f50ef4f9d2f7b49174f60baff04dad4737b
bhupendrasinghacme <bhupendrasingh@acmeintech.in>
2022-10-29 12:36:14 +0530

    bhupendra 29 oct

---
+++ public/Sw.js
@@ -0,0 +1,24 @@
+let cacheData = "doctorV1";
+this.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheData).then((cache) => {
+            cache.addAll([
+                "/static/js/bundle.js",
+                "/static/js/main.chunk.js",
+                "/static/js/0.chunk.js",
+                "/index.html",
+                "/"
+            ]);
+        })
+    )
+});
+
+this.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((result) => {
+            if (result) {
+                return result;
+            }
+        })
+    )
+})
